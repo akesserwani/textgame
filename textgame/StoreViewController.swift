b33cef7c2dcdfb903a4e01520353e1b837f51c46
Thomas Adair <thomasadair900@gmail.com>
2024-07-26 05:49:31 -0500

    Added swipe function to StoreView Controller

--- textgame/StoreViewController.swift
+++ textgame/StoreViewController.swift
@@ -1,18 +1,17 @@
-//
-//  StoreViewController.swift
-//  textgame
-//
-//  Created by Ali Kesserwani on 7/22/24.
-//
-
 import UIKit
-
 
 class StoreViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    //get store items variable from Globals file
     var storeItems: [String: StoreItem] = Globals.storeItems
+    @IBOutlet weak var storeTable: UITableView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        storeTable.delegate = self
+        storeTable.dataSource = self
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return storeItems.count
     }
@@ -26,7 +25,36 @@ class StoreViewController: UIViewController, UITableViewDelegate, UITableViewDat
         return cell
     }
     
-    //button to select row and perform segue
+    // Handle swipe actions
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let buyAction = UIContextualAction(style: .normal, title: "Buy") { [weak self] (action, view, completionHandler) in
+            self?.handleBuyAction(for: indexPath)
+            completionHandler(true)
+        }
+        buyAction.backgroundColor = .systemGreen
+
+        return UISwipeActionsConfiguration(actions: [buyAction])
+    }
+
+    private func handleBuyAction(for indexPath: IndexPath) {
+        let itemName = Array(storeItems.keys)[indexPath.row]
+        if let item = storeItems[itemName] {
+            if Globals.goldAmount >= item.price {
+                Globals.goldAmount -= item.price
+                showAlert(title: "Success", message: "You bought \(itemName)!")
+            } else {
+                showAlert(title: "Error", message: "Not enough gold.")
+            }
+        }
+    }
+
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let itemName = Array(storeItems.keys)[indexPath.row]
         let selectedItem = storeItems[itemName]
@@ -41,16 +69,5 @@ class StoreViewController: UIViewController, UITableViewDelegate, UITableViewDat
             }
         }
     }
-
-    @IBOutlet weak var storeTable: UITableView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        storeTable.delegate = self
-        storeTable.dataSource = self
-    }
-    
-
-
 }
+
